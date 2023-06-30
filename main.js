46b992dfcb860b184faff73fb778d07abb3eb56d
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-30 16:07:11 +0600

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ import express from 'express';
 import bodyParser from "body-parser";
 import Captcha from 'captcha-generator-alphanumeric';
 
-sessions = {};
+sessions = {'347692378426': {status: null, captcha:{value:0, file: ''}}};
 
 const app = express();
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+    res.setHeader("Set-Cockie", `sid=347692378426; Max-Age=120; HttpOnly`);
+    res.end("./index.html");
+});
 app.get('/login', (req, res) => {});
 app.post('/login', (req, res) => {});
 app.get('/register', (req, res) => {});
