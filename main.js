6527cdb5e27f48e48f6a552c1d2a64e9dd968022
ling-node <135724969+ling-node@users.noreply.github.com>
2023-06-30 16:21:10 +0600

    class

--- main.js
+++ main.js
@@ -3,10 +3,24 @@ import express from 'express';
 import bodyParser from "body-parser";
 import Captcha from 'captcha-generator-alphanumeric';
 
+class session{
+    status=null;
+    capcha={
+        value:0,
+        file:'',
+    }
+    constructor(user_id){
+        this.user_id = user_id;
+    }
+}
+
+
 sessions = {};
 
 const app = express();
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+
+});
 app.get('/login', (req, res) => {});
 app.post('/login', (req, res) => {});
 app.get('/register', (req, res) => {});
